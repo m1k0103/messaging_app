365cd8dfd3f29e8a547b6eef8a1fde2c435ea1b3
mikok <84585119+miko1034@users.noreply.github.com>
2024-09-22 16:44:00 +0100

    edited route

--- messaging_app/routes.py
+++ messaging_app/routes.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, jsonify, request
+from messaging_app.func import Database, get_db_name
+
+db = Database(get_db_name())
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
-def send_message():
+@app.route("/", methods=["GET"])
+def home():
     if request.method == "GET":
         return render_template("main.html")
     
-    elif request.method == "POST":
-        return "posted"
-
-    else:
-        return "en error occured"
+@app.route("/send",methods=["POST"])
+def send_message():
+    pass
