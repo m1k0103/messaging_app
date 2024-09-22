b4f784c3eb0f6231b1b121206d9cae04583fc38a
mikok <84585119+miko1034@users.noreply.github.com>
2024-09-22 21:22:04 +0100

    edited home,send_mesage,get_messages routes

--- messaging_app/routes.py
+++ messaging_app/routes.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session, url_for
 from messaging_app.func import Database, get_db_name
 
 db = Database(get_db_name())
 
 app = Flask(__name__)
+app.secret_key = "secret_key" # not secure lol
 
-@app.route("/", methods=["GET"])
+
+@app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "GET":
+        #previous_20_messages = 
         return render_template("main.html")
-    
-@app.route("/send",methods=["POST"])
+    elif request.method == "POST":
+        nickname = request.form["nickname"]
+        db.create_user(nickname,"")
+        session["nickname"] = nickname
+        return render_template("main.html")
+
+@app.route("/sendmessage",methods=["POST"])
 def send_message():
-    pass
+    user = session["nickname"]
+    message = request.form["message"]
+    print(user,message)
+    db.add_message_to_db(user,message)
+    return render_template("main.html")
+
+@app.route("/getmessages",methods=["GET"])
+def get_messages():
+    messages = db.get_last_messages(20)
+    return render_template("messages.html", messages_list=messages)
+    
